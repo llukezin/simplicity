9d2a35f92c55fe5f6a0fe8009307f64ea6fe7db3
llukezin <lucassmendonca77@gmail.com>
2023-10-25 15:24:12 -0300

    colocando o ligth

--- JS/contato.js
+++ JS/contato.js
@@ -76,3 +76,39 @@ botaoBuscar.addEventListener("click", async function (event){
 
 
 });
+
+
+
+
+/* Programação do Formspree */
+
+
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: formulario.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Seus dados foram enviados! Aguarde retorno.";
+      formulario.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Deu ruim! algo de errado não está certo!"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+formulario.addEventListener("submit", handleSubmit)
